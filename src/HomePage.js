5923fe7028cdf53f546b29d09df6ee608fdd6680
AhmedSI <asimk2015@yahoo.com>
2020-07-29 02:13:17 +0200

    some styling in the editor page, loading during segmentation added, removing redundt download button, logo added

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,12 @@
 import React, {Component } from "react";
-import "./HomePage.css";
 import "tui-image-editor/dist/tui-image-editor.css";
+import "./HomePage.css";
 import ImageEditor from "@toast-ui/react-image-editor";
 import Button from "react-bootstrap/Button";
 import {Row,Container} from "react-bootstrap";
 import { withRouter } from 'react-router';
 import Axios from 'axios';
+import Loader from 'react-loader-spinner';
 
 const icona = require("tui-image-editor/dist/svg/icon-a.svg");
 const iconb = require("tui-image-editor/dist/svg/icon-b.svg");
@@ -13,12 +14,10 @@ const iconc = require("tui-image-editor/dist/svg/icon-c.svg");
 const icond = require("tui-image-editor/dist/svg/icon-d.svg");
 const download = require("downloadjs");
 const myTheme = {
-  'common.bi.image': '',
+  // 'common.bi.image': '',
   'common.border': '0px',
-  'common.backgroundColor': '#1e1e1e',
-  'common.bisize.height': '30px',
-  "menu.backgroundColor": "white",
-  "common.backgroundColor": "#151515",
+  'common.bisize.height': '0px',
+  "common.backgroundColor": "#414551",
   "downloadButton.backgroundColor": "white",
   "downloadButton.borderColor": "white",
   "downloadButton.color": "black",
@@ -26,13 +25,25 @@ const myTheme = {
   "menu.activeIcon.path": iconb,
   "menu.disabledIcon.path": icona,
   "menu.hoverIcon.path": iconc,
+  'menu.normalIcon.color': '#d25a46',
+  'menu.activeIcon.color': '#eb5f4b',
+  'menu.disabledIcon.color': '#dbbdb9',
+  'menu.hoverIcon.color': '#e6887b',
+  'submenu.normalIcon.color': '#8a8a8a',
+  'submenu.activeIcon.color': '#e9e9e9',
+  'menu.iconSize.width': '20px',
+  'menu.iconSize.height': '20px',
+  'loadButton.backgroundColor': '#eb5f4b',
+  'loadButton.color': '#414551',
+  'loadButton.fontSize': '20px',
 };
 
 class HomePage extends Component {
   
   state={
     imageSrc:'', 
-    setImageSrc:""
+    setImageSrc:"",
+    loading:false
   }
   
   onDidMount = () => {
@@ -76,50 +87,61 @@ class HomePage extends Component {
   segmentPost= ()=>{
     const imageEditorInst = this.imageEditor.current.imageEditorInst;
     const data = imageEditorInst.toDataURL();
-    document.getElementsByClassName("tui-image-editor-load-btn")[1].value = '';
-    Axios.post('https://segmentor.azurewebsites.net', {img:data}).then(response=>{
-
-      imageEditorInst.loadImageFromFile(this.dataURLtoFile(response.data.res, 'lena'));
+    const that = this;
+    this.setState({loading:true},()=>{
+      Axios.post('https://segmentor.azurewebsites.net', {img:data}).then(response=>{
+        imageEditorInst.loadImageFromFile(this.dataURLtoFile(response.data.res, 'lena')).then(()=>{
+          this.setState({loading:false});
+        });
+      });
     });
   }
 
   render(){
     return (
       <div className="home-page">
-        <div className="center">
-          <h1>Welcome to Harmonies photo Editor</h1>
-          <Button className='button' onClick={this.saveImageToDisk}>Save Image to Disk</Button>
-          <Button style={{marginLeft:'50px'}} className='button' onClick={this.segmentPost}>Segment</Button>
-          <Button style={{marginLeft:'50px'}} className='button' onClick={this.magicLoad}>Harmonize</Button>
-        </div>
-        <Container>
-          <Row>
+        {this.state.loading===false?
+          <div className="center"><h1>Welcome to Harmonies photo Editor</h1>
+            <Button className='button' onClick={this.saveImageToDisk}>Save Image to Disk</Button>
+            <Button style={{marginLeft:'50px'}} className='button' onClick={this.segmentPost}>Segment</Button>
+            <Button style={{marginLeft:'50px'}} className='button' onClick={this.magicLoad}>Harmonize</Button>
+          </div>
+          :<div></div>
+        }
+        <div style={{display:this.state.loading===false? "flex":"none", justifyContent: "center", alignItems: "center"}} >
           <ImageEditor
-                includeUI={{
-                  loadImage: {
-                    path: this.state.imageSrc,
-                    name: "image",
-                  },
-                  theme: myTheme,
-                  menu: ["crop", "flip", "rotate", "draw", "shape", "text", "filter"],
-                  initMenu: "",
-                  uiSize: {
-                    height: `calc(79vh)`,
-                    width: `calc(250vh)`
-                  },
-                  menuBarPosition: "left",
-                }}
-                cssMaxHeight={window.innerHeight}
-                cssMaxWidth={window.innerWidth}
-                selectionStyle={{
-                  cornerSize: 20,
-                  rotatingPointOffset: 70,
-                }}
-                usageStatistics={true}
-                ref={this.imageEditor}
-              />
-          </Row>
-        </Container>
+            includeUI={{
+              loadImage: {
+                path: this.state.imageSrc,
+                name: "image",
+              },
+              theme: myTheme,
+              menu: ["crop", "flip", "rotate", "draw", "shape", "text", "filter"],
+              initMenu: "",
+              uiSize: {
+                height: `calc(77vh)`,
+                width: `calc(150vh)`
+              },
+              menuBarPosition: "left",
+            }}
+            cssMaxHeight={window.innerHeight}
+            cssMaxWidth={window.innerWidth}
+            selectionStyle={{
+              cornerSize: 20,
+              rotatingPointOffset: 70,
+            }}
+            usageStatistics={true}
+            ref={this.imageEditor}
+          />
+        </div>
+        <div>
+            <div style={{display:this.state.loading===true?"flex":"none", justifyContent: "center", alignItems: "center"}}>
+              <img src="harmonies-logo-2.png" height='300px' />
+            </div>
+            <div style={{width: "100%", height: "100", display:this.state.loading===true?"flex":"none", justifyContent: "center", alignItems: "center" }}>
+              <Loader type="Grid" color="#d25a46" height="100" width="100" />
+            </div>
+          </div>
       </div>
     );
   }
--- src/TopBar.js
+++ src/TopBar.js
@@ -7,7 +7,7 @@ function TopBar({ location }) {
   const { pathname } = location;
   return (
     <Navbar expand="lg" variant="dark">
-      <Navbar.Brand href="#home" style={{color:'#eb5f4b',fontWeight:'bold'}}>Harmoines</Navbar.Brand>
+      <Navbar.Brand href="#home" style={{color:'#eb5f4b',fontWeight:'bold'}}><img src="harmonies-logo-1.png" height="50px" style={{marginRight:"5px"}}/>Harmoines</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
